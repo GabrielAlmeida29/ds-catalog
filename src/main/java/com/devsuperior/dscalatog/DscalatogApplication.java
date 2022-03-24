d405e5d4a74b27d4ea9cfc374e92e0c7ca1af2e6
Gabriel Santos de Almeida <1gabrielsantosdealmeida@gmail.com>
2022-03-24 15:46:05 -0300

    Backend Project Created

---
+++ src/main/java/com/devsuperior/dscalatog/DscalatogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscalatog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscalatogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscalatogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dscalatog/DscalatogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscalatog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscalatogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
