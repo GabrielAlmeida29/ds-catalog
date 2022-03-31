705658b7e84c45677f22990363bf0ff849ef527a
Gabriel Santos de Almeida <1gabrielsantosdealmeida@gmail.com>
2022-03-31 19:26:35 -0300

    DTO

--- src/main/java/com/devsuperior/dscalatog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscalatog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscalatog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscalatog.dto.CategoryDTO;
 import com.devsuperior.dscalatog.entities.Category;
 import com.devsuperior.dscalatog.repositories.CategoryRepository;
 
@@ -16,8 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
