6db9574062538f6207654f42d259deae1c68ead1
Gabriel Santos de Almeida <1gabrielsantosdealmeida@gmail.com>
2022-03-31 18:14:18 -0300

    Database Seeding

--- src/main/java/com/devsuperior/dscalatog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscalatog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscalatog.entities.Category;
 import com.devsuperior.dscalatog.repositories.CategoryRepository;
@@ -13,9 +14,10 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
+
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
-	
+
 }
