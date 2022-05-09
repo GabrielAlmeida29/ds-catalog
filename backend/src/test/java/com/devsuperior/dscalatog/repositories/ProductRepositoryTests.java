4bcea113fe59d7c7bee92789dbf7ff055087b4c0
Gabriel Santos de Almeida <1gabrielsantosdealmeida@gmail.com>
2022-05-09 19:17:18 -0300

    First Test (Repository)

---
+++ backend/src/test/java/com/devsuperior/dscalatog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscalatog.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+	@Test
+	public void deleteShouldDeletObjectWhenIdExist() {
+		
+	}
+}
